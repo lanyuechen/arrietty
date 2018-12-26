9228771b4ae236f99130b6c92b0062ba51246487
zhenglipeng <1252012805@qq.com>
2018-12-26 19:18:23 +0800

    添加测试

--- src/formula/formula.test.js
+++ src/formula/formula.test.js
@@ -1,7 +1,21 @@
 import Formula from './index';
 
 describe('Formula', () => {
-  it('test', () => {
-    expect(1).toBe(1);
+  it('transform', () => {
+    expect(Formula.explain('1 + 2 + 3').join(', ')).toBe('1, 2, +, 3, +');
+    expect(Formula.explain('1 + (2 + 3)').join(', ')).toBe('1, 2, 3, +, +');
+    expect(Formula.explain('1 * 2 + 3').join(', ')).toBe('1, 2, *, 3, +');
+    expect(Formula.explain('1 * (2 + 3)').join(', ')).toBe('1, 2, 3, +, *');
+    expect(Formula.explain('(1 + 2) * (3 + 4) + 5').join(', ')).toBe('1, 2, +, 3, 4, +, *, 5, +');
+    expect(Formula.explain('1 + (2 + (3 + (4 + 5))').join(', ')).toBe('1, 2, 3, 4, 5, +, +, +, +');
+  });
+
+  it('calc', () => {
+    expect(Formula.result('1 + 2 + 3')).toBe(6);
+    expect(Formula.result('1 + (2 + 3)')).toBe(6);
+    expect(Formula.result('1 * 2 + 3')).toBe(5);
+    expect(Formula.result('1 * (2 + 3)')).toBe(5);
+    expect(Formula.result('(1 + 2) * (3 + 4) + 5')).toBe(26);
+    expect(Formula.result('1 + (2 + (3 + (4 + 5))')).toBe(15);
   });
 });
