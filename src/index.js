8ff9ad1d22503f8603084251d6cd429347fdba0a
zhenglipeng <1252012805@qq.com>
2019-01-24 11:27:58 +0800

    添加statis模块

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export Event from './event';
 export Formula from './formula';
 export Explain from './explain';
 export Collection from './collection';
+export Statis from './statis';
 
 export Deep from './deep';
 
